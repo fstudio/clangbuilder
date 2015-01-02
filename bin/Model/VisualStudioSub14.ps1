116b151dbd52d1c67dc0a2ff37ec5b67ecae6e70
Force Charlie <charlieio@outlook.com>
2015-01-02 13:36:52 +0800

    Add VisualStudio Environment Sub file models

---
+++ bin/Model/VisualStudioSub14.ps1
@@ -0,0 +1,12 @@
+<#############################################################################
+#  VisualStudioSub14.ps1
+#  Note: Clang Auto Build Environment for Visual Studio 2015
+#  Data:2015.01.02
+#  Author:Force <forcemz@outlook.com>    
+##############################################################################>
+
+IF( $env:VS140COMNTOOLS -eq $null -and (Test-Path $env:VS140COMNTOOLS -eq $false))
+{
+  Write-Host -ForegroundColor Red "Not Fond Vaild Install for Visual Studio 2015"
+  return ;
+}
