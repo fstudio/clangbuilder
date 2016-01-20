b81a54b3cfc7c4c04aa747ee5127f0f167340053
Force Charlie <charlieio@outlook.com>
2016-01-20 13:34:21 +0800

    Add Package.lock.json Lock Version Feature

--- Packages/RestorePackages.ps1
+++ Packages/RestorePackages.ps1
@@ -9,6 +9,20 @@ $SelfFolder=$PSScriptRoot;
 Push-Location $PWD
 Set-Location $SelfFolder
 
+Function Expand-ZipPackage
+{
+    param(
+        [Parameter(Position=0,Mandatory=$True,HelpMessage="Unzip sources")]
+        [ValidateNotNullorEmpty()]
+        [String]$ZipSource,
+        [Parameter(Position=1,Mandatory=$True,HelpMessage="Output Directory")]
+        [ValidateNotNullorEmpty()]
+        [String]$Destination
+    )
+    [System.Reflection.Assembly]::LoadWithPartialName('System.IO.Compression.FileSystem')|Out-Null
+    [System.IO.Compression.ZipFile]::ExtractToDirectory($ZipSource, $Destination)
+}
+
 Import-Module -Name BitsTransfer
 $IsWindows64=[System.Environment]::Is64BitOperatingSystem
 
@@ -28,28 +42,41 @@ $NSISSub="nsis-3.0b3"
 
 $GnuWinURL="http://sourceforge.net/projects/clangonwin/files/Install/Packages/ClangSetup-Package-GnuWin-win32.zip"
 
-<#$CMakeLock="3.4.1"
-$SubversionLock="1.9.3"
-$PythonLock="2.7.11"
-$NSISLock="3.0b3"
-$GnuWinLock="1.0"
-#>
 
-Function Expand-ZipPackage
-{
-    param(
-        [Parameter(Position=0,Mandatory=$True,HelpMessage="Unzip sources")]
-        [ValidateNotNullorEmpty()]
-        [String]$ZipSource,
-        [Parameter(Position=1,Mandatory=$True,HelpMessage="Output Directory")]
-        [ValidateNotNullorEmpty()]
-        [String]$Destination
-    )
-    [System.Reflection.Assembly]::LoadWithPartialName('System.IO.Compression.FileSystem')|Out-Null
-    [System.IO.Compression.ZipFile]::ExtractToDirectory($ZipSource, $Destination)
+
+
+$PackageMap=@{}
+$PackageMap["CMake"]="3.4.2"
+$PackageMap["Subversion"]="1.9.3"
+$PackageMap["Python"]="2.7.11"
+$PackageMap["NSIS"]="3.0b3"
+$PackageMap["GNUWin"]="1.0"
+
+#### Test Package.lock.json
+$PackageLockJson=$null
+#$PackageLockJsonNew=ConvertTo-Json $PackageMap|ConvertFrom-Json
+
+if(Test-Path "$SelfFolder/Package.lock.json"){
+    $PackageLockJson=Get-Content -TotalCount -1 -Path "$SelfFolder/Package.lock.json" |ConvertFrom-Json
 }
 
-
+if($PackageLockJson -ne $null){
+    if($PackageLockJson.CMake -ne $PackageMap["CMake"] -and (Test-Path "$SelfFolder\CMake") ){
+        Rename-Item "$SelfFolder\CMake" "$SelfFolder\CMake.bak"
+    }
+    if($PackageLockJson.Subversion -ne $PackageMap["Subversion"] -and (Test-Path "$SelfFolder\Subversion")){
+        Rename-Item "$SelfFolder\Subversion" "$SelfFolder\Subversion.bak"
+    }
+    if($PackageLockJson.Python -ne $PackageMap["Python"] -and (Test-Path "$SelfFolder\Python")){
+        Rename-Item "$SelfFolder\Python" "$SelfFolder\Python.bak"
+    }
+    if($PackageLockJson.NSIS -ne $PackageMap["NSIS"] -and (Test-Path "$SelfFolder\NSIS")){
+        Rename-Item "$SelfFolder\NSIS" "$SelfFolder\NSIS.bak"
+    }
+    if($PackageLockJson.GNUWin -ne $PackageMap["GNUWin"] -and (Test-Path "$SelfFolder\GNUWin")){
+        Rename-Item "$SelfFolder\GNUWin" "$SelfFolder\GNUWin.bak"
+    }
+}
 
 if(!(Test-Path "$SelfFolder/cmake/bin/cmake.exe")){
     Write-Output "Download CMake and Unzip CMake"
@@ -141,6 +168,41 @@ if(!(Test-Path "$SelfFolder/GNUWin/bin/grep.exe")){
     Write-Output  "GNUWin has been installed"
 }
 
-Pop-Location
+##Check Package
+if(!(Test-Path "$SelfFolder\CMake")){
+    if(Test-Path "$SelfFolder\CMake.bak"){
+        Rename-Item "$SelfFolder\CMake.bak" "$SelfFolder\CMake"
+    }
+}
 
+if(!(Test-Path "$SelfFolder\Python")){
+    if(Test-Path "$SelfFolder\Python.bak"){
+        Rename-Item "$SelfFolder\Python.bak" "$SelfFolder\Python"
+    }
+}
+
+if(!(Test-Path "$SelfFolder\Subversion")){
+    if(Test-Path "$SelfFolder\Subversion.bak"){
+        Rename-Item "$SelfFolder\Subversion.bak" "$SelfFolder\Subversion"
+    }
+}
+
+if(!(Test-Path "$SelfFolder\NSIS")){
+    if(Test-Path "$SelfFolder\NSIS.bak"){
+        Rename-Item "$SelfFolder\NSIS.bak" "$SelfFolder\NSIS"
+    }
+}
+
+if(!(Test-Path "$SelfFolder\GNUWin")){
+    if(Test-Path "$SelfFolder\GNUWin.bak"){
+        Rename-Item "$SelfFolder\GNUWin.bak" "$SelfFolder\GNUWin"
+    }
+}
+
+Remove-Item -Recurse -Force "$SelfFolder\*.bak"
+
+##Write Lock File
+ConvertTo-Json $PackageMap |Out-File -Force -FilePath "$SelfFolder\Package.lock.json"
+
+Pop-Location
 Write-Output "Your can Load PathLoader to Setting Your Clangbuilder Environment"
