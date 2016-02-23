87788be629e48d5c3718a955d93d1be3a6582e6d
Force Charlie <charlieio@outlook.com>
2016-02-23 22:12:36 +0800

    RestoreLLDBRequired add Python27 sources down

--- bin/RestoreLLDBRequired.ps1
+++ bin/RestoreLLDBRequired.ps1
@@ -4,7 +4,6 @@
 #  Date:2016 01
 #  Author:Force <forcemz@outlook.com>
 ##############################################################################>
-$SelfFolder=$PSScriptRoot;
 Import-Module -Name BitsTransfer
 
 # PowerShell 5.0 :Expand-Archive
@@ -19,20 +18,63 @@ Function Expend-ZipPackage
         [String]$Folder
     )
     [System.Reflection.Assembly]::LoadWithPartialName('System.IO.Compression.FileSystem')|Out-Null
-    Write-Host "Use System.IO.Compression.ZipFile Unzip nPackage: $Source`nOutput: $Folder"
+    Write-Host "Use System.IO.Compression.ZipFile Unzip `nPackage: $Source`nOutput: $Folder"
     [System.IO.Compression.ZipFile]::ExtractToDirectory($Source, $Folder)
 }
 
-Function Get-SWigWIN{
-$SWIGWINURL="http://sourceforge.net/projects/swig/files/swigwin/swigwin-3.0.8/swigwin-3.0.8.zip"
-Start-BitsTransfer -Source $SWIGWINURL -Destination "$SelfFolder\Required\swigwin.zip" -Description "Downloading swigwin"
-if(Test-Path "$SelfFolder\Required\swigwin.zip"){
-    Unblock-File -Path "$SelfFolder\Required\swigwin.zip"
-    Expend-ZipPackage -Source "$SelfFolder\Required\swigwin.zip" -Folder "$SelfFolder\Required"
-    Rename-Item "$SelfFolder\Required\swigwin-3.0.8" "$SelfFolder\Required\swigwin"
-}else{
-    Write-Error "Download swigwin failure!"
-}
+Function Restore-Python{
+    param(
+        [Parameter(Position=0,Mandatory=$True,HelpMessage="Enter URL")]
+        [ValidateNotNullorEmpty()]
+        [String]$URL,
+        [String]$Folder="$PSScriptRoot\Required"
+    )
+    Start-BitsTransfer -Source $URL -Destination "python27.zip" -Description "Downloading Python 27 sources"
+    if(Test-Path "python27.zip"){
+        Unblock-File -Path "python27.zip"
+        Expend-ZipPackage -Source "python27.zip" -Folder $Folder
+        Rename-Item "$PSScriptRoot\Required\python27-master" "$PSScriptRoot\Required\Python27"
+        Remove-Item -Force "python27.zip"
+    }else{
+        Write-Error "Download Python 27 sources failure !"
+    }
 }
 
-Get-SWigWIN
+Function Restore-Swigwin{
+    param(
+        [Parameter(Position=0,Mandatory=$True,HelpMessage="Enter URL")]
+        [ValidateNotNullorEmpty()]
+        [String]$URL,
+        [String]$Folder="$PSScriptRoot\Required"
+    )
+    Start-BitsTransfer -Source $URL -Destination "swigwin.zip" -Description "Downloading swigwin"
+    if(Test-Path "swigwin.zip"){
+        Unblock-File -Path "swigwin.zip"
+        Expend-ZipPackage -Source "swigwin.zip" -Folder $Folder
+        Rename-Item "$PSScriptRoot\Required\swigwin-3.0.8" "$PSScriptRoot\Required\swigwin"
+        Remove-Item -Force "swigwin.zip"
+    }else{
+        Write-Error "Download swigwin failure !"
+    }
+}
+
+
+$RequiredFolder="$PSScriptRoot\Required"
+$SwigwinUrl="http://sourceforge.net/projects/swig/files/swigwin/swigwin-3.0.8/swigwin-3.0.8.zip"
+$PythonUrl="https://github.com/fstudio/python27/archive/master.zip"
+
+Push-Location $PWD
+Set-Location $RequiredFolder
+
+if(!(Test-Path "$RequiredFolder\swigwin\swig.exe")){
+    Restore-Swigwin -URL $SwigwinUrl 
+}
+
+if(!(Test-Path "$RequiredFolder\Python27")){
+    Restore-Python -URL $PythonUrl
+}
+
+$env:PATH=$env:PATH+";"+"$RequiredFolder"
+
+
+
---
+++ packages/Restore7zip.ps1
@@ -0,0 +1,7 @@
+#
+#
+Function Expend-MsiPackage{
+}
+
+$szipx86Url="http://www.7-zip.org/a/7z1514.msi"
+$szipx64Url="http://www.7-zip.org/a/7z1514-x64.msi"
