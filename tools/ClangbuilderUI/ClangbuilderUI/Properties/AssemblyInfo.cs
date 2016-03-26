87455ae99ba28036398e18d2ec02972aa6e44c11
Force Charlie <charlieio@outlook.com>
2016-03-26 21:59:28 +0800

    ClangBuilderUI AssemblyInfo.cs update

--- tools/ClangbuilderUI/ClangbuilderUI/Properties/AssemblyInfo.cs
+++ tools/ClangbuilderUI/ClangbuilderUI/Properties/AssemblyInfo.cs
@@ -7,7 +7,7 @@ using System.Windows;
 // 有关程序集的常规信息通过以下
 // 特性集控制。更改这些特性值可修改
 // 与程序集关联的信息。
-[assembly: AssemblyTitle("Clangbuilder UI vNext")]
+[assembly: AssemblyTitle("Clangbuilder UI Utility")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Force Studio")]
