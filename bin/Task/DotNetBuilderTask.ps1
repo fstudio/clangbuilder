4312b8a8377a46606fd20aaee422fd03dda2b953
Force Charlie <charlieio@outlook.com>
2016-04-17 18:17:44 +0800

    Add Write-Logger Modules

---
+++ bin/Task/DotNetBuilderTask.ps1
@@ -0,0 +1,7 @@
+<#
+# DotNet Builder Task
+#>
+
+$CoreCLRUrl="https://github.com/dotnet/coreclr.git"
+$CoreRTUrl="https://github.com/dotnet/corert.git"
+$LLVMilcUrl="https://github.com/dotnet/llilc.git"
