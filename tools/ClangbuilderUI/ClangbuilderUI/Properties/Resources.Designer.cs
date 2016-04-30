40784f2c388c806421b1c51e147b450c4ef9a1b5
Force Charlie <charlieio@outlook.com>
2016-04-30 23:06:22 +0800

    ClangBuilderUI rename

--- tools/ClangbuilderUI/ClangbuilderUI/Properties/Resources.Designer.cs
+++ tools/ClangbuilderUI/ClangbuilderUI/Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace ClangSetupvNextSet.Properties
+namespace ClangbuilderUI.Properties
 {
 
 
