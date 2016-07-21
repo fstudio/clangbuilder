cfecb6da16f5af99f87f1d7636d65dc2f4c59a58
Force Charlie <charlieio@outlook.com>
2016-07-21 08:22:12 +0800

    update VisualCpptools URL

--- tools/RestoreVisualCppBuildToolsFull.ps1
+++ tools/RestoreVisualCppBuildToolsFull.ps1
@@ -3,4 +3,5 @@
 
 #https://blogs.msdn.microsoft.com/vcblog/2016/03/31/announcing-the-official-release-of-the-visual-c-build-tools-2015/
 #http://download.microsoft.com/download/5/d/6/5d6a1865-11ff-41f8-8af6-60e92d9bbd0b/visualcppbuildtools_full.exe
-$VisualCppBuildToolsFullExe="https://download.microsoft.com/download/3/8/E/38EE4758-7C31-4D96-8FF9-83CC313F0F78/VisualCppBuildTools_Full.exe"
+#http://landinghub.visualstudio.com/visual-cpp-build-tools
+$VisualCppBuildToolsFullExe="https://download.microsoft.com/download/5/f/7/5f7acaeb-8363-451f-9425-68a90f98b238/visualcppbuildtools_full.exe"
