abfd9ceef695f7b06700179d57beda6efc6f7a2d
Force Charlie <charlieio@outlook.com>
2016-09-10 22:16:22 +0800

    Rewrite WindowManager

--- bin/View/WindowManager.ps1
+++ bin/View/WindowManager.ps1
@@ -5,21 +5,21 @@
 #  Author:Force <forcemz@outlook.com>    
 ##############################################################################>
 
-
-
-Function Global:Show-LauncherWindowUI{
+Function Global:Show-LauncherWindow{
+    param(
+        [int]$Timeout=1
+    )
 $xaml = @"
 <Window
  xmlns='http://schemas.microsoft.com/winfx/2006/xaml/presentation'>
-
- <Border BorderThickness="25" BorderBrush="White" CornerRadius="9" Background='White'>
+ <Border BorderThickness="25" BorderBrush="White" CornerRadius="100" Background='White'>
   <StackPanel>
    <Label FontSize="50" FontFamily='Segoe UI' Background='White' Foreground='Black' BorderThickness='1'>
-    This Report 
+    Clangbuilder
    </Label>
-
-   <Label HorizontalAlignment="Center" FontSize="15" FontFamily='Consolas' Background='Aqua' Foreground='Black' BorderThickness='0'>
-    ClangSetup vNext
+   <Label HorizontalAlignment="Center" FontSize="15" FontFamily='Segoe UI' 
+    Background='Aqua' Foreground='Black' BorderThickness='0'>
+    Clangbuilder Initializing ...
    </Label>
   </StackPanel>
  </Border>
@@ -28,21 +28,30 @@ $xaml = @"
 Add-Type -assemblyName PresentationFramework
 $reader = [System.XML.XMLReader]::Create([System.IO.StringReader] $xaml)
 $window = [System.Windows.Markup.XAMLReader]::Load($reader)
-$Window.AllowsTransparency = $True
+$window.AllowsTransparency = $True
+#$window.Icon="$PSScriptRoot/Notifications.ico"
 $window.SizeToContent = 'WidthAndHeight'
 $window.ResizeMode = 'NoResize'
-$Window.Opacity = .7
+
+$Mebs=Get-Member -InputObject $window
+
+# foreach($i in $Mebs){
+#     Write-Host $i
+# }
+
+#$Window.Opacity = .9
 $window.Topmost = $true
-$window.WindowStartupLocation = 'Manual'
+$window.WindowStartupLocation = 'CenterScreen'
 $window.WindowStyle = 'None'
 # show message for 5 seconds:
 #$Host.UI.RawUI.WindowPosition
 $null = $window.Show()
-Start-Sleep -Seconds 2
+Start-Sleep -Milliseconds $Timeout 
 $window.Close()
 }
 
-Show-LauncherWindowUI
+
+Show-LauncherWindow -Timeout 500
 
 
 
