7f9f073b23b3f0bcea8b8503bb3243ca0ce9ae2d
Force Charlie <charlieio@outlook.com>
2017-09-15 17:07:53 +0800

    CMakeInstallationFix

--- modules/CMake/CMake.psm1
+++ modules/CMake/CMake.psm1
@@ -9,6 +9,6 @@ Function CMakeInstallationFix {
     Get-ChildItem -Path $TargetDir  -Recurse *.cmake | Foreach-Object {
         $content = Get-Content $_.FullName
         $content = $content.Replace("`$(Configuration)", "$Configuration")
-        $content|Out-File $_.FullName
+        $content|Out-File $_.FullName -Encoding utf8
     }
 }
