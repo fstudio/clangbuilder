f164a8d07d84999600f8ab0409686b1e99aa5709
Force Charlie <charlieio@outlook.com>
2018-04-03 22:00:29 +0800

    support load config/profile.ps1

--- bin/ProfileEnv.ps1
+++ bin/ProfileEnv.ps1
@@ -4,4 +4,8 @@
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 $ClangbuilderRoot = Split-Path -Parent $PSScriptRoot
 $Pkglocksdir = "$ClangbuilderRoot/bin/pkgs/.locks"
+if(Test-Path "$ClangbuilderRoot/config/profile.ps1"){
+    ."$ClangbuilderRoot/config/profile.ps1"
+}
+
 Write-Debug $Pkglocksdir
