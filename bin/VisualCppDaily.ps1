ab1fabfa96239e8af0c113def5c3218657b3c005
Force Charlie <charlieio@outlook.com>
2018-04-05 19:22:14 +0800

    [visualcpp]: VisualCppTools.Community.Daily.VS2017Layout use nuget api v3

--- bin/VisualCppDaily.ps1
+++ bin/VisualCppDaily.ps1
@@ -1,13 +1,46 @@
 # See https://blogs.msdn.microsoft.com/vcblog/2016/02/16/\
 #try-out-the-latest-c-compiler-toolset-without-waiting-for-the-next-update-of-visual-studio/
 ."$PSScriptRoot\ProfileEnv.ps1"
-
-$ViusalCppAtomURL = "https://visualcpp.myget.org/F/dailymsvc/api/v2"
+$VisualCppDailyAPI = "https://visualcpp.myget.org/F/dailymsvc/api/v3/index.json"
+$VisualCppDailyName = "VisualCppTools.Community.Daily.VS2017Layout"
 $VisualCppToolsInstallDir = "$ClangbuilderRoot\bin\utils\msvc"
-
 $NuGetDir = "$ClangbuilderRoot\bin\pkgs\Nuget"
-
 $env:PATH = "$NuGetDir;${env:PATH}"
+$VisualCppDailyVersion = $null
+# https://visualcpp.myget.org/F/dailymsvc/api/v3/query/?q=packageid:VisualCppTools.Community.Daily.VS2017Layout&prerelease=true
+
+try {
+    $queryurl = $null
+    $obj = (Invoke-WebRequest -Uri $VisualCppDailyAPI).Content |ConvertFrom-Json
+    foreach ($s in $obj.resources) {
+        if ($s.'@type' -eq "SearchQueryService") {
+            $queryurl = $s.'@id'
+        }
+    }
+    if ($queryurl -eq $null) {
+        Write-Host -ForegroundColor Red "SearchQueryService url not found"
+        exit 1
+    }
+    $surl = "$queryurl/?q=packageid:$VisualCppDailyName&prerelease=true"
+    $vcobj = (Invoke-WebRequest -Uri $surl).Content |ConvertFrom-Json
+    if ($vcobj.data -is [array]) {
+        $VisualCppDailyVersion = $vcobj.data[0].version
+    }
+    else {
+        $VisualCppDailyVersion = $vcobj.data.version
+    }
+
+}
+catch {
+    Write-Host -ForegroundColor Red "Check VisualCppTools Latest version error: $_"
+    exit 1
+}
+
+if ($VisualCppDailyVersion -eq $null) {
+    Write-Host -ForegroundColor Red "Not found valid $VisualCppDailyName version."
+    exit 1
+}
+
 
 Push-Location $PWD
 
@@ -17,67 +50,23 @@ if (!(Test-Path $VisualCppToolsInstallDir)) {
 
 Set-Location $VisualCppToolsInstallDir
 
-Function CompareVersion() {
-    param(
-        [String]$Pre,
-        [String]$Next
-    )
-
-}
-
-$xmlfeed = $null
-try {
-    $xmlfeed = [xml](Invoke-WebRequest -UseBasicParsing -Uri "$ViusalCppAtomURL/Packages").Content
-}
-catch {
-    Write-Host -Forceground Red "Checking VisualCpp Feed $_"
-    exit 1
-}
-
-
-$VisualCppPackageName = $null
-$VisualCppToolsVersion = $null
-
-if ($xmlfeed.feed.entry.GetType().IsArray) {
-    [int]$index = 0;
-    [int]$mindex = 0;
-    [int]$build = 1;
-    foreach ($_ in $xmlfeed.feed.entry) {
-        $version = $_.properties.Version.Split("-")[0]; # 14.11.25615-Pre
-        $ver = [System.Version]::Parse($version)
-        if ($ver.build -gt $build) {
-            $build = $ver.build
-            $mindex = $index
-        }
-        $index++
-    }
-    $VisualCppPackageName = $xmlfeed.feed.entry[$mindex].properties.Id
-    $VisualCppToolsVersion = $xmlfeed.feed.entry[$mindex].properties.Version
-}
-else {
-    $VisualCppPackageName = $xmlfeed.feed.entry.properties.Id
-    $VisualCppToolsVersion = $xmlfeed.feed.entry.properties.Version
-}
-
-Write-Output "Latest $VisualCppPackageName version is $VisualCppToolsVersion"
+Write-Output "Latest $VisualCppDailyName version is $VisualCppDailyVersion"
 if ((Test-Path "$VisualCppToolsInstallDir/VisualCppTools.lock.json")) {
     $Pkglock = Get-Content "$VisualCppToolsInstallDir/VisualCppTools.lock.json" |ConvertFrom-Json
-    if ($Pkglock.Version -eq $VisualCppToolsVersion) {
-        Write-Host "VisualCppTools is up to date, Version: $VisualCppToolsVersion"
+    if ($Pkglock.Version -eq $VisualCppDailyVersion) {
+        Write-Host "VisualCppTools is up to date, Version: $VisualCppDailyVersion"
         return ;
     }
 }
 
+Write-Output "NuGet Install $VisualCppDailyName $VisualCppDailyVersion ......"
+&nuget install $VisualCppDailyName -Version $VisualCppDailyVersion -Pre -Source $VisualCppDailyAPI
 
-Write-Output "NuGet Install $VisualCppPackageName $VisualCppToolsVersion ......"
-
-&nuget install $VisualCppPackageName -Source $ViusalCppAtomURL -Prerelease
-
-if ((Test-Path "$VisualCppToolsInstallDir\$VisualCppPackageName.$VisualCppToolsVersion")) {
+if ((Test-Path "$VisualCppToolsInstallDir\$VisualCppDailyName.$VisualCppDailyVersion")) {
     $vccache = @{}
-    $vccache["Name"] = $VisualCppPackageName
-    $vccache["Version"] = $VisualCppToolsVersion
-    $vccache["Path"] = "$VisualCppPackageName.$VisualCppToolsVersion"
+    $vccache["Name"] = $VisualCppDailyName
+    $vccache["Version"] = $VisualCppDailyVersion
+    $vccache["Path"] = "$VisualCppDailyName.$VisualCppDailyVersion"
     ConvertTo-Json $vccache |Out-File -Encoding utf8 -Force -FilePath "$VisualCppToolsInstallDir\VisualCppTools.lock.json"
 }
 
