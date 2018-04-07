9b6bf98b606d75790277b13fa28909065dc537c8
Force Charlie <charlieio@outlook.com>
2018-04-07 14:13:06 +0800

    [clangbuilder] libcxx use LIBCXX_NO_VCRUNTIME

--- bin/ClangbuilderTarget.ps1
+++ bin/ClangbuilderTarget.ps1
@@ -192,7 +192,7 @@ Function Get-ClangArgument {
         $Arguments += " -DLLVM_FORCE_BUILD_RUNTIME=ON -DLIBCXX_ENABLE_SHARED=YES"
         $Arguments += " -DLIBCXX_ENABLE_STATIC=YES -DLIBCXX_ENABLE_EXPERIMENTAL_LIBRARY=ON"
         #$Arguments += " -DLIBCXX_ENABLE_FILESYSTEM=ON"
-        $Arguments += " -DLIBCXX_HAS_WIN32_THREAD_API=ON"
+        $Arguments += " -DLIBCXX_HAS_WIN32_THREAD_API=ON -DLIBCXX_NO_VCRUNTIME=ON"
     }
     return $Arguments
 }
