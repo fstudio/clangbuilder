5e593cfb7a1a7c42c1c23d68203569c66defed1a
Force Charlie <charlieio@outlook.com>
2018-04-26 07:10:20 +0800

    [blast] code adjust

--- sources/blast/blast.hpp
+++ sources/blast/blast.hpp
@@ -6,6 +6,59 @@
 #include <Windows.h>
 #include <string>
 
+/*
+#define IO_REPARSE_TAG_MOUNT_POINT              (0xA0000003L)
+#define IO_REPARSE_TAG_HSM                      (0xC0000004L)
+#define IO_REPARSE_TAG_HSM2                     (0x80000006L)
+#define IO_REPARSE_TAG_SIS                      (0x80000007L)
+#define IO_REPARSE_TAG_WIM                      (0x80000008L)
+#define IO_REPARSE_TAG_CSV                      (0x80000009L)
+#define IO_REPARSE_TAG_DFS                      (0x8000000AL)
+#define IO_REPARSE_TAG_SYMLINK                  (0xA000000CL)
+#define IO_REPARSE_TAG_DFSR                     (0x80000012L)
+#define IO_REPARSE_TAG_DEDUP                    (0x80000013L)
+#define IO_REPARSE_TAG_NFS                      (0x80000014L)
+#define IO_REPARSE_TAG_FILE_PLACEHOLDER         (0x80000015L)
+#define IO_REPARSE_TAG_WOF                      (0x80000017L)
+#define IO_REPARSE_TAG_WCI                      (0x80000018L)
+#define IO_REPARSE_TAG_WCI_1                    (0x90001018L)
+#define IO_REPARSE_TAG_GLOBAL_REPARSE           (0xA0000019L)
+#define IO_REPARSE_TAG_CLOUD                    (0x9000001AL)
+#define IO_REPARSE_TAG_CLOUD_1                  (0x9000101AL)
+#define IO_REPARSE_TAG_CLOUD_2                  (0x9000201AL)
+#define IO_REPARSE_TAG_CLOUD_3                  (0x9000301AL)
+#define IO_REPARSE_TAG_CLOUD_4                  (0x9000401AL)
+#define IO_REPARSE_TAG_CLOUD_5                  (0x9000501AL)
+#define IO_REPARSE_TAG_CLOUD_6                  (0x9000601AL)
+#define IO_REPARSE_TAG_CLOUD_7                  (0x9000701AL)
+#define IO_REPARSE_TAG_CLOUD_8                  (0x9000801AL)
+#define IO_REPARSE_TAG_CLOUD_9                  (0x9000901AL)
+#define IO_REPARSE_TAG_CLOUD_A                  (0x9000A01AL)
+#define IO_REPARSE_TAG_CLOUD_B                  (0x9000B01AL)
+#define IO_REPARSE_TAG_CLOUD_C                  (0x9000C01AL)
+#define IO_REPARSE_TAG_CLOUD_D                  (0x9000D01AL)
+#define IO_REPARSE_TAG_CLOUD_E                  (0x9000E01AL)
+#define IO_REPARSE_TAG_CLOUD_F                  (0x9000F01AL)
+#define IO_REPARSE_TAG_CLOUD_MASK               (0x0000F000L)
+#define IO_REPARSE_TAG_APPEXECLINK              (0x8000001BL)
+#define IO_REPARSE_TAG_PROJFS                   (0x9000001CL)
+#define IO_REPARSE_TAG_STORAGE_SYNC             (0x8000001EL)
+#define IO_REPARSE_TAG_WCI_TOMBSTONE            (0xA000001FL)
+#define IO_REPARSE_TAG_UNHANDLED                (0x80000020L)
+#define IO_REPARSE_TAG_ONEDRIVE                 (0x80000021L)
+#define IO_REPARSE_TAG_PROJFS_TOMBSTONE         (0xA0000022L)
+#define IO_REPARSE_TAG_AF_UNIX                  (0x80000023L)
+
+/// Windows Linux Subsystem
+#define IO_REPARSE_TAG_LX_FIFO                  (0x80000024L)
+#define IO_REPARSE_TAG_LX_CHR                   (0x80000025L)
+#define IO_REPARSE_TAG_LX_BLK                   (0x80000026L)
+*/
+
+#ifndef IO_REPARSE_TAG_APPEXECLINK
+#define IO_REPARSE_TAG_APPEXECLINK (0x8000001BL)
+#endif
+
 // https://docs.microsoft.com/en-us/windows-hardware/drivers/ddi/content/ntifs/ns-ntifs-_reparse_data_buffer
 
 typedef struct _REPARSE_DATA_BUFFER {
@@ -54,7 +107,7 @@ typedef struct _REPARSE_DATA_BUFFER {
       //-- FILE_PROVIDER_EXTERNAL_INFO_V1 --------------------
       ULONG FileInfo_Version; // Should be 1 (FILE_PROVIDER_CURRENT_VERSION)
       ULONG
-          FileInfo_Algorithm; // Usually 0 (FILE_PROVIDER_COMPRESSION_XPRESS4K)
+      FileInfo_Algorithm; // Usually 0 (FILE_PROVIDER_COMPRESSION_XPRESS4K)
     } WofReparseBuffer;
 
     // Structure for IO_REPARSE_TAG_APPEXECLINK
