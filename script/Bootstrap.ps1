8d1627bad8bd60b8a0ea55f4bc75a28ef9692271
Force Charlie <charlieio@outlook.com>
2018-04-29 22:03:01 +0800

    [env] bootstrap

--- script/Bootstrap.ps1
+++ script/Bootstrap.ps1
@@ -2,18 +2,30 @@
 # TOO BOOTSTRAP ENVIRONMENT
 
 # Check vswhere is exists.
-$Cbroot=Split-Path $PSScriptRoot
-$vswherebin="$Cbroot\bin\pkgs\vshwere\vswhere.exe"
+$Cbroot = Split-Path $PSScriptRoot
+$vswherebin = "$Cbroot\bin\pkgs\vswhere\vswhere.exe"
 
-Function Fetchvswhere{
-    try{
-
-    }catch{
-        Write-Host "$_"
+Function Fetchvswhere {
+    try {
+        [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+        $json=Get-Content "$Cbroot\ports\vswhere.json"|ConvertFrom-Json
+        $InternalUA = [Microsoft.PowerShell.Commands.PSUserAgent]::Chrome
+        New-Item -ItemType Directory "$env:TEMP\vswhere.$PID"|Out-Null
+        Invoke-WebRequest -Uri $json.url -OutFile "$TEMP\vswhere.$PID\vswhere.exe" -UserAgent $InternalUA -UseBasicParsing
+        $env:PATH="$env:PATH;$env:TEMP\vswhere.$PID"
+    }
+    catch {
+        Write-Host "download vswhere error$_"
+        exit 1
     }
 }
 
 
-if(!(Test-Path $vswherebin)){
+if (Test-Path $vswherebin) {
+    $env:PATH="$env:PATH;$Cbroot\bin\pkgs\vshwere"
+}else{
     Write-Host "try download vswhere $vswherebin"
+    Fetchvswhere
 }
+
+Invoke-Expression "$Cbroot\bin\CompileUtils.ps1"
--- script/InitializeEnv.bat
+++ script/InitializeEnv.bat
@@ -2,6 +2,9 @@
 title "Initialize Clangbuilder"
 if not exist "%~dp0../bin/blast.exe" (
 	PowerShell -NoProfile -NoLogo -ExecutionPolicy unrestricted -File "%~dp0Bootstrap.ps1"
+	call "%~dp0DevAll.bat"
+) else (
+	call "%~dp0DevAll.bat"
+	call "%~dp0CompileUI.bat"
 )
-call "%~dp0DevAll.bat"
-call "%~dp0CompileUI.bat"
+
