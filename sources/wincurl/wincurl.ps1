feda75980d781a9f7adb698d5c8a8effd5855c17
Force Charlie <charlieio@outlook.com>
2018-09-13 22:33:19 +0800

    [tools] add win curl build script, working in progress

---
+++ sources/wincurl/wincurl.ps1
@@ -0,0 +1,8 @@
+#!/usr/bin/env pwsh
+# Require Clangbuilder install perl
+# build zlib static
+# build openssl static
+# build libssh2 static ?
+# build brotli static
+# build curl static
+# download curl-ca-bundle.crt
