08d731b1fd9a25507f5d6f7b1908ba2ed173c6ee
Force Charlie <charlieio@outlook.com>
2018-09-14 22:36:05 +0800

    [tools] wincurl update

--- sources/wincurl/wincurl.ps1
+++ sources/wincurl/wincurl.ps1
@@ -6,6 +6,7 @@ $OPENSSL_VERSION = "1.1.1"
 $BROTLI_VERSION = "1.0.5"
 $LIBSSH2_VERSION = "1.8.0"
 
+# thanks https://github.com/curl/curl-for-win
 
 # Filename
 $ZLIB_FILENAME = "zlib-${ZLIB_VERSION}"
@@ -18,6 +19,33 @@ $LIBSSH2_URL = "https://github.com/libssh2/libssh2/releases/download/libssh2-${L
 Write-Verbose $ZLIB_URL $OPENSSL_URL $BROTLI_URL $LIBSSH2_URL
 
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+
+
+Function Exec {
+    param(
+        [string]$FilePath,
+        [string]$Argv,
+        [string]$WD
+    )
+    $ProcessInfo = New-Object System.Diagnostics.ProcessStartInfo
+    $ProcessInfo.FileName = $FilePath
+    Write-Host "$FilePath $Argv $WD"
+    if ($null -eq $WD -or $WD.Length -eq 0) {
+        $ProcessInfo.WorkingDirectory = $PWD
+    }
+    else {
+        $ProcessInfo.WorkingDirectory = $WD
+    }
+    $ProcessInfo.Arguments = $Argv
+    $ProcessInfo.UseShellExecute = $false ## use createprocess not shellexecute
+    $Process = New-Object System.Diagnostics.Process
+    $Process.StartInfo = $ProcessInfo
+    if ($Process.Start() -eq $false) {
+        return -1
+    }
+    $Process.WaitForExit()
+    return $Process.ExitCode
+}
 
 $Global:Subffix = ""
 if ($IsDesktop -or $IsWindows) {
@@ -88,7 +116,7 @@ if ($null -eq $cmakeexe) {
 Write-Host -ForegroundColor Green "Find cmake install: $cmakeexe"
 
 
-$Ninjaexe=Findcommand -Name "ninja"
+$Ninjaexe = Findcommand -Name "ninja"
 if ($null -eq $Ninjaexe) {
     Write-Host -ForegroundColor Red "Please install ninja."
     return 1
@@ -96,32 +124,32 @@ if ($null -eq $Ninjaexe) {
 
 Write-Host -ForegroundColor Green "Find cmake install: $Ninjaexe"
 
-$Patchexe=Findcommand -Name "patch"
-if($null -eq $Patchexe){
-    $Gitexe=Findcommand -Name "git"
+$Patchexe = Findcommand -Name "patch"
+if ($null -eq $Patchexe) {
+    $Gitexe = Findcommand -Name "git"
     if ($null -eq $Gitexe) {
         Write-Host -ForegroundColor Red "Please install git for windows (or PortableGit)."
         return 1
     }
-    $gitinstall=Split-Path -Parent (Split-Path -Parent $gitexe)
-    if([String]::IsNullOrEmpty($gitinstall)){
+    $gitinstall = Split-Path -Parent (Split-Path -Parent $gitexe)
+    if ([String]::IsNullOrEmpty($gitinstall)) {
         Write-Host -ForegroundColor Red "Please install git for windows (or PortableGit)."
         return 1
     }
-    $patchx=Join-Path $gitinstall "usr/bin/patch.exe"
-    if(!(Test-Path $patchx)){
-        $xinstall=Split-Path -Parent $gitinstall
-        if([String]::IsNullOrEmpty($xinstall)){
+    $patchx = Join-Path $gitinstall "usr/bin/patch.exe"
+    if (!(Test-Path $patchx)) {
+        $xinstall = Split-Path -Parent $gitinstall
+        if ([String]::IsNullOrEmpty($xinstall)) {
             Write-Host -ForegroundColor Red "Please install git for windows (or PortableGit)."
             return 1
         }
-        $patchx=Join-Path  $xinstall "usr/bin/patch.exe"
-        if(!(Test-Path $patchx)){
+        $patchx = Join-Path  $xinstall "usr/bin/patch.exe"
+        if (!(Test-Path $patchx)) {
             Write-Host -ForegroundColor Red "Please install git for windows (or PortableGit)."
             return 1
         }
     }
-    $Patchexe=$pathx
+    $Patchexe = $pathx
 }
 
 Write-Host  -ForegroundColor Green "Found patch install: $Patchexe"
@@ -131,8 +159,8 @@ Function DecompressTar {
     param(
         [String]$File
     )
-    $p = Start-Process -FilePath $tarexe -ArgumentList "$decompress $File" -Wait -NoNewWindow -PassThru
-    if ($p.ExitCode -ne 0) {
+    $exitcode = Exec -FilePath $tarexe -Argv "$decompress $File"
+    if ($exitcode -ne 0) {
         return $false
     }
     return $true
@@ -152,29 +180,48 @@ if (!(DecompressTar -File "$ZLIB_FILENAME.tar.gz")) {
     Write-Host -ForegroundColor Red "Decompress $ZLIB_FILENAME.tar.gz failed!"
     exit 1
 }
+
+Write-Host -ForegroundColor Yellow "Apply zlib.patch ..."
 # Apply patch
 $ZLIB_PACTH = "$PSScriptRoot/zlib.patch"
 
-Set-Location "$ZLIB_FILENAME"
+#Set-Location "$ZLIB_FILENAME"
 
-&"$Pacthexe" -Nbp1 -i $ZLIB_PACTH
-
-Set-Location ".."
+$ec = Exec -FilePath $Pacthexe -Argv "-Nbp1 -i `"$ZLIB_PACTH`"" -WD "$PWD/$ZLIB_FILENAME"
+if ($ec -ne 0) {
+    Write-Host -ForegroundColor Red "Apply $ZLIB_PACTH failed"
+}
 
 if (!(MkdirAll -Dir "zlib_build")) {
     exit 1
 }
 
-Set-Location "zlib_build"
-cmake -GNinja -DCMAKE_BUILD_TYPE=Release`
--DCMAKE_INSTALL_PREFIX=$Prefix `
-    -DSKIP_INSTALL_FILES=ON `
-    -DSKIP_BUILD_EXAMPLES=ON `
-    -DBUILD_SHARED_LIBS=OFF `
-    "../$ZLIB_FILENAME"
-## TODO build
-ninja all
-ninja install
+$cmakeflags = "-GNinja " + `
+    "-DCMAKE_BUILD_TYPE=Release " + `
+    "`"-DCMAKE_INSTALL_PREFIX=$Prefix`" " + `
+    "`"-DCMAKE_C_FLAGS=-MT`"" +`
+    "`"-DCMAKE_CXX_FLAGS=-MT`"" +`
+    "-DSKIP_INSTALL_FILES=ON -DSKIP_BUILD_EXAMPLES=ON " + `
+    "-DBUILD_SHARED_LIBS=OFF `"$PWD/$ZLIB_FILENAME`""
+
+
+$ec = Exec-FilePath $cmakeexe -Argv $cmakeflags -WD "$PWD/zlib_build"
+if ($ec -ne 0) {
+    Write-Host -ForegroundColor Red "zlib: create build.ninja error"
+    return 1
+}
+
+$ec = Exec -FilePath $Ninjaexe -Argv "all" -WD "$PWD/zlib_build" 
+if ($ec -ne 0) {
+    Write-Host -ForegroundColor Red "zlib: build error"
+    return 1
+}
+
+$ec = Exec -FilePath $Ninjaexe -Argv "install" -WD "$PWD/zlib_build"
+if ($ec -ne 0) {
+    Write-Host -ForegroundColor Red "zlib: install error"
+    return 1
+}
 
 
 # build zlib static
