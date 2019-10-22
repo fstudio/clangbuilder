f599f2b2b714ffdb15fc818e82a70dace798fa12
Force Charlie <charlieio@outlook.com>
2019-10-22 16:48:00 +0800

    [wincurl] update ca to 2019-10-16

--- sources/wincurl/version.ps1
+++ sources/wincurl/version.ps1
@@ -39,7 +39,7 @@ $CURL_URL = "https://curl.haxx.se/download/curl-${CURL_VERSION}.tar.gz"
 $CURL_FILE = "curl-${CURL_VERSION}"
 
 #curl-ca-bundle
-$CA_BUNDLE_URL = "https://curl.haxx.se/ca/cacert-2019-08-28.pem"
+$CA_BUNDLE_URL = "https://curl.haxx.se/ca/cacert-2019-10-16.pem"
 
 Function DumpLocal {
     $dumptext = $ZLIB_VERSION + $ZLIB_HASH + $ZLIB_FILENAME + $ZLIB_URL 
