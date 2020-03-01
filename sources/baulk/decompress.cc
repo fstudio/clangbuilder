3f91b38a0e1e9747ea112fe1991a593c7f75d6c8
Force.Charlie <charlieio@outlook.com>
2020-03-01 08:41:00 +0800

    [baulk] adjust code

--- sources/baulk/decompress.cc
+++ sources/baulk/decompress.cc
@@ -1,12 +1,14 @@
 #include "baulk.hpp"
+#include "decompress.hpp"
 
 namespace baulk {
 //
-
-bool DecompressFile(std::wstring_view src, std::wstring_view destdir,
-                    bela::error_code &ec) {
-  //
-  return false;
+namespace exe {
+bool Decompress(std::wstring_view src, std::wstring_view outdir,
+                bela::error_code &ec) {
+  return true;
 }
+bool Regularize(std::wstring_view path) { return true; }
+} // namespace exe
 
 } // namespace baulk
