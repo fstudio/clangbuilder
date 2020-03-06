9f407a980a08a2decff913f28cd507feed0cea38
Force.Charlie <charlieio@outlook.com>
2020-03-06 22:34:46 +0800

    [ports] update cmake to 3.16.5

--- sources/baulk/jsonex.hpp
+++ sources/baulk/jsonex.hpp
@@ -14,6 +14,24 @@ inline bool BindTo(nlohmann::json &j, std::string_view name,
   }
   return false;
 }
+
+template <typename Integer>
+inline bool BindToInteger(nlohmann::json &j, std::string_view name,
+                          Integer &i) {
+  if (auto it = j.find(name); it != j.end()) {
+    b = it->get<Integer>();
+    return true;
+  }
+  return false;
+}
+
+inline bool BindTo(nlohmann::json &j, std::string_view name, bool &b) {
+  if (auto it = j.find(name); it != j.end()) {
+    b = it->get<bool>();
+    return true;
+  }
+  return false;
+}
 } // namespace baulk::json
 
 #endif
