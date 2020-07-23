b6f1de739a82d372a7e18cbb4baeb879dbc82b76
Force Charlie <6904176+fcharlie@users.noreply.github.com>
2020-07-23 14:05:25 +0800

    [wincurl] update ca bundle url

--- sources/wincurl/version.ps1
+++ sources/wincurl/version.ps1
@@ -40,7 +40,7 @@ $CURL_URL = "https://curl.haxx.se/download/curl-${CURL_VERSION}.tar.gz"
 $CURL_FILE = "curl-${CURL_VERSION}"
 
 #curl-ca-bundle
-$CA_BUNDLE_URL = "https://curl.haxx.se/ca/cacert-2020-06-24.pem"
+$CA_BUNDLE_URL = "https://curl.haxx.se/ca/cacert-2020-07-22.pem"
 
 Function DumpLocal {
     $dumptext = $ZLIB_VERSION + $ZLIB_HASH + $ZLIB_FILENAME + $ZLIB_URL 
